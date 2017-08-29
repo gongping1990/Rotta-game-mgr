61029499f4f47958045b76df8cd2c731965dc909
gp <gp646890523@sina.com>
2017-08-29 19:41:48 +0800

    update( login ) 验证码后台获取

--- src/components/generalcomponent/login.vue
+++ src/components/generalcomponent/login.vue
@@ -15,8 +15,9 @@
         <el-form-item label="验证码" prop="captcha">
           <el-input class="input codeInput" placeholder="请输入" v-model="userinfo.captcha"></el-input>
           <!--<a href="#" class="getBtn" v-if="isgetCode === false" @click="changGetcode($event,true)">获取验证码</a>-->
-          <div class="codeImgBox" @click="changGetcode">
-            <canvas id="canvasCode" width="120" height="36" ></canvas>
+          <div class="code-imgbox" @click="changGetcode">
+            <el-button class="code-btn" type="text" v-if="!userinfo.copyCode">获取验证码</el-button>
+            <img v-else class="code-img" :src="userinfo.copyCode">
           </div>
         </el-form-item>
         <el-form-item class="justfy3">
@@ -96,90 +97,8 @@
       }
     },
     mounted: function () {
-      this.canvasChangGetcode()
     },
     methods: {
-      canvasChangGetcode (e) {
-        let str = this.userinfo.copyCode || '获取验证码'
-        // 绘制验证码
-        var drawPic = () => {
-          var canvas = document.getElementById('canvasCode')
-          console.log(canvas)
-          var width = canvas.width
-          var height = canvas.height
-          var ctx = canvas.getContext('2d')
-          ctx.textBaseline = 'bottom'
-          // 绘制背景色
-          ctx.fillStyle = randomColor(180, 240) // 颜色若太深可能导致看不清
-          ctx.fillRect(0, 0, width, height)
-
-          if (str === '获取验证码') {
-            // 绘制文字
-            for (var o = 0; o < 5; o++) {
-              var txt1 = Array.from(str.toString())[o]
-              ctx.fillStyle = randomColor(50, 160) // 随机生成字体颜色
-              ctx.font = 20 + 'px SimHei' // 随机生成字体大小
-              var x1 = 10 + o * 25
-              var y1 = randomNum(25, 45)
-              var deg1 = randomNum(-45, 45)
-              // 修改坐标原点和旋转角度
-              ctx.translate(x1, y1)
-              ctx.rotate(deg1 * Math.PI / 180)
-              ctx.fillText(txt1, 0, 0)
-              // 恢复坐标原点和旋转角度
-              ctx.rotate(-deg1 * Math.PI / 180)
-              ctx.translate(-x1, -y1)
-            }
-          } else {
-            // 绘制文字
-            for (var i = 0; i < 4; i++) {
-              var txt = Array.from(str.toString())[i]
-              ctx.fillStyle = randomColor(50, 160) // 随机生成字体颜色
-              ctx.font = randomNum(15, 40) + 'px SimHei' // 随机生成字体大小
-              var x = 10 + i * 25
-              var y = randomNum(25, 45)
-              var deg = randomNum(-45, 45)
-              // 修改坐标原点和旋转角度
-              ctx.translate(x, y)
-              ctx.rotate(deg * Math.PI / 180)
-              ctx.fillText(txt, 0, 0)
-              // 恢复坐标原点和旋转角度
-              ctx.rotate(-deg * Math.PI / 180)
-              ctx.translate(-x, -y)
-            }
-
-            // 绘制干扰线
-            for (var k = 0; k < 4; k++) {
-              ctx.strokeStyle = randomColor(40, 180)
-              ctx.beginPath()
-              ctx.moveTo(randomNum(0, width), randomNum(0, height))
-              ctx.lineTo(randomNum(0, width), randomNum(0, height))
-              ctx.stroke()
-            }
-            // 绘制干扰点
-            for (var j = 0; j < 50; j++) {
-              ctx.fillStyle = randomColor(0, 255)
-              ctx.beginPath()
-              ctx.arc(randomNum(0, width), randomNum(0, height), 1, 0, 2 * Math.PI)
-              ctx.fill()
-            }
-          }
-        }
-
-        function randomNum (min, max) {
-          return Math.floor(Math.random() * (max - min) + min)
-        }
-
-        /** 生成一个随机色 **/
-        function randomColor (min, max) {
-          var r = randomNum(min, max)
-          var g = randomNum(min, max)
-          var b = randomNum(min, max)
-          return 'rgb(' + r + ' , ' + g + ' , ' + b + ')'
-        }
-
-        drawPic()
-      },
       changGetcode () {
         var forCode = {}
         forCode.usage = 'login'
@@ -194,11 +113,10 @@
             const [err, ret] = result
             if (err) {
             } else {
-              var code = ret.data.payload.code
+              var code = ret.data.payload
               // console.log('验证码是:', code)
-              this.userinfo.copyCode = code
+              this.userinfo.copyCode = `data:image/png;base64,${code}`
             }
-            this.canvasChangGetcode()
           }
         )
         this.isgetCode = true
@@ -268,20 +186,15 @@
 .login-left{flex: 0.6;text-align: center;padding-top: 20%;}
 .login-right{flex: 0.4;padding-top: 17%;}
 .title-big{font-size: 4rem;}
-.title-small{font-size: 1.2rem;padding-left: 23rem;padding-top: 0.5rem;}
 
 .input{width: 20rem;}
-.codeInput{width:120px;overflow: auto}
-/* .input1{width: 11rem;position: relative;top: -2rem;} */
-.verifyimg{display: inline-block;height: 4rem;width: 8.2rem;margin-left: 0.75rem;position: relative;top: -1rem;}
-.botton{background-color: #919191;color: #fff;}
-.botton:hover{color: #49a9ee;border-color: #fff;}
+.codeInput{width:120px;overflow: auto;float: left}
+.code-img{width:100%; height: 34px;border: 1px solid #eee; border-radius: 2px}
+.code-btn{text-align: center;width: 100%}
 .justfy1{margin-right: 11rem;}
 .justfy2{margin-left: 1.2rem;}
 .justfy3{}
-.justfy4{margin-left: -2.3rem}
 /**/
-.getBtn{text-decoration: none;cursor: pointer;text-align: center; overflow:auto;color: #108ee9;width: 120px;height: 36px;
-display: inline-block;line-height: 36px;background-color: #e8e8e8}
-.codeImgBox{display: inline-block;cursor: pointer;background-color: #eee; height:36px}
+
+.code-imgbox{width: 120px; height: 34px;margin-left: 0.15rem;float: left;}
 </style>
